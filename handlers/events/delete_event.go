592e3de955a4d648d10f6d433612beaaaed85433
Maria Khelli <13520115@std.stei.itb.ac.id>
2023-12-04 12:59:16 +0700

    [Feat] Delete endpoint handler

--- handlers/events/delete_event.go
+++ handlers/events/delete_event.go
@@ -1,8 +1,19 @@
 package events
 
 import (
+	"backend-go/fs"
+	"backend-go/repository"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteEvent(c *gin.Context) {
+	id := c.Param("id")
+	err := repository.DeleteEvent(fs.CTX, fs.FSClient, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "message": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "message": "Event deleted successfully"})
 }
